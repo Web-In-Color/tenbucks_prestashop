9d324d877d9df06dbc2a33ecee4be480563f04f2
Gary PEGEOT <gary@webincolor.fr>
2015-12-04 16:16:13 +0100

    Add translations for french

---
+++ translations/fr.php
@@ -0,0 +1,15 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{tenbucks}prestashop>tenbucks_f56f94e720061edd59304c6881cf015d'] = 'tenbucks';
+$_MODULE['<{tenbucks}prestashop>tenbucks_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Désinstaller ce module ?';
+$_MODULE['<{tenbucks}prestashop>tenbucks_9077d6f8da66e7e0db8a4989f8924935'] = 'Mes applications';
+$_MODULE['<{tenbucks}prestashop>tenbucks_d95cf4ab2cbf1dfb63f066b50558b07d'] = 'Mon compte';
+$_MODULE['<{tenbucks}prestashop>tenbucks_32812fd6a91e0cf2836a8152ce5b0932'] = 'Le service web est désactivé, veuillez l\'activer pour utiliser nos services.';
+$_MODULE['<{tenbucks}prestashop>tenbucks_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuration';
+$_MODULE['<{tenbucks}prestashop>tenbucks_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{tenbucks}prestashop>tenbucks_f38f5974cdc23279ffe6d203641a8bdf'] = 'Paramètres mis à jour.';
+$_MODULE['<{tenbucks}prestashop>tenbucks_bd4332512e3fae04205aba6b54f06453'] = 'Nouvelle clé : %s.';
+$_MODULE['<{tenbucks}prestashop>tenbuckscontroller_5ce4845f88a46b76777d42ff4f6a60bf'] = 'Plein écran';
+$_MODULE['<{tenbucks}prestashop>tenbuckscontroller_f38f5974cdc23279ffe6d203641a8bdf'] = 'Paramètres mis à jour.';
